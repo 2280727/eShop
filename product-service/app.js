84001eefc9e7d55a25415f9514ebf1061d7abacc
2280727 <p.creator44@gmail.com>
2025-04-12 10:39:42 +0200

    nebale cors

--- product-service/app.js
+++ product-service/app.js
@@ -2,11 +2,13 @@ import express from 'express';
 import sequelize from './db.js';
 import productRouter from './routes/productRouter.js'
 import { NotFoundError } from './errors/index.js';
+import cors from 'cors'
 
 
 const app = express();
 
 app.use(express.json());
+app.use(cors());
 
 app.use('/products', productRouter);
 
