128dc4214a5467ca348661053469decae085b958
2280727 <p.creator44@gmail.com>
2025-04-16 16:03:53 +0200

    update: redis url

--- product-service/connections/redis.js
+++ product-service/connections/redis.js
@@ -4,7 +4,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 
 const redisClient = createClient({
-    url: process.env.REDIS_URL,
+    url: 'redis://135.181.150.234:6379',
 });
 
 redisClient.on('error', (err) => console.error('Redis Client Error', err));
