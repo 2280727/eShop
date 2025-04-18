9855a01c96b7cec14dc8800c0b30b34653869e7c
2280727 <p.creator44@gmail.com>
2025-04-18 13:43:32 +0200

    chore: add api documentation, add service name to the logs

--- product-service/app.js
+++ product-service/app.js
@@ -5,7 +5,8 @@ import productRouter from './routes/productRouter.js'
 import { NotFoundError } from './errors/index.js';
 import cors from 'cors'
 import { rateLimiter } from './middlewares/rate-limiter.js';
-
+import swaggerUi from 'swagger-ui-express';
+import swaggerDocument from './swagger.json' assert { type: "json" };
 
 initTracing({
     serviceName: 'product_service',
@@ -18,7 +19,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use(rateLimiter)
-app.use('/products', productRouter);
+app.use('/v1/products', productRouter);
 
 app.use((error, req, res, next) => {
     console.error("Error", error)
@@ -32,7 +33,7 @@ app.use((error, req, res, next) => {
         error: "Oops something went wrong, try again later"
     })
 })
-
+app.use('/api-docs', swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 
 sequelize.sync();
 
--- product-service/monitor/tracing.js
+++ product-service/monitor/tracing.js
@@ -4,28 +4,31 @@ import { NodeSDK } from '@opentelemetry/sdk-node';
 import { getNodeAutoInstrumentations } from '@opentelemetry/auto-instrumentations-node';
 import { OTLPTraceExporter } from '@opentelemetry/exporter-trace-otlp-http';
 import { SemanticResourceAttributes } from '@opentelemetry/semantic-conventions';
-import otelResources from '@opentelemetry/resources';
-const { Resource } = otelResources;
+import resources from '@opentelemetry/resources';
+const { Resource } = resources;
 
 export function initTracing({ serviceName, collectorUrl }) {
   const traceExporter = new OTLPTraceExporter({
     url: collectorUrl
   });
 
+  const resource = new Resource({
+    [SemanticResourceAttributes.SERVICE_NAME]: serviceName
+  });
+
   const sdk = new NodeSDK({
     traceExporter,
     instrumentations: [getNodeAutoInstrumentations()],
-    // resource: new Resource({
-    //   [SemanticResourceAttributes.SERVICE_NAME]: serviceName
-    // })
+    resource
   });
 
-try {
-    sdk.start(); // Start the SDK synchronously
+  try {
+    sdk.start();
     console.log('✅ OpenTelemetry initialized');
-} catch (error) {
+  } catch (error) {
     console.error('Error initializing OpenTelemetry', error);
-}
+  }
+
   const shutdown = async () => {
     try {
       await sdk.shutdown();
